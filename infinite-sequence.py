27d102100b1c0144df2b6afd92fd46a43e56254c
Vlad <vladisgrig@yandex.ru>
2016-10-09 01:04:39 +0300

    solve all issues

--- infinite-sequence.py
+++ infinite-sequence.py
@@ -1,5 +1,3 @@
-import re
-
 def get_position(number):
     """Возвращает позицию числа в бесконечной последовательности"""
     num_length = len(str(number))
@@ -19,71 +17,98 @@ def check_rightshift(sub, last_elem):
 
 def check_main(sub, step, start):
     """Функция проверяет, сформирована ли последовательность числами"""
-    while (start + step) < len(sub):
-        first_item = sub[start:start + step]
-        first_item_num = int(first_item)
-        second_item_num = first_item_num + 1
-        second_item = str(second_item_num)
-        if (start + step + len(second_item)) < len(sub):
-            if sub[start + step: start + step + len(second_item)] == second_item:
-                start += step
-                step = len(second_item)
+    if (start + step) <= len(sub):
+        while (start + step) <= len(sub):
+            first_item = sub[start:start + step]
+            first_item_num = int(first_item)
+            second_item_num = first_item_num + 1
+            second_item = str(second_item_num)
+            if (start + step + len(second_item)) <= len(sub):
+                if sub[start + step:start + step + len(second_item)] == second_item:
+                    start += step
+                    step = len(second_item)
+                else:
+                    break
             else:
-                break
+                right_part = sub[start + step:]
+                if right_part:
+                    if check_rightshift(right_part, first_item):
+                        return True
+                    else:
+                        return False
+                else:
+                    return True
         else:
-            right_part = sub[start + step:]
-            if check_rightshift(right_part, first_item):
-                return True
-            else:
-                return False
+            return True
+        return False
     else:
         return True
-    return False
 
 def check_leftshift(sub, step, shift):
     """Функция проверяет может ли левый сдвиг быть частью предыдущего числа"""
-    first_item = sub[shift:shift + step]
-    first_item_num = int(first_item)
-    zero_item_num = first_item_num - 1
-    zero_item = str(zero_item_num)
-    if sub[:shift] == zero_item[-shift:]:
-        return (True, zero_item_num)
+    if sub[shift:shift+1] != '0':
+        if (shift + step) <= len(sub):
+            first_item = sub[shift:shift + step]
+            first_item_num = int(first_item)
+            zero_item_num = first_item_num - 1
+            zero_item = str(zero_item_num)
+            if sub[:shift] == zero_item[-shift:]:
+                return (True, zero_item_num)
+            else:
+                return (False,)
+        else:
+            part_of_first_item = sub[shift:]
+            part_of_zero_item = sub[:shift]
+            index = 0
+            for i in part_of_zero_item:
+                if i == '0':
+                    index += 1
+                else:
+                    break
+            else:
+                index -= 1
+            right = int(part_of_zero_item[index:]) + 1
+            if len(str(right)) > len(part_of_zero_item[index:]):
+                part_of_zero_item_plus_one = part_of_zero_item[:index-1] + str(right)
+            else:
+                part_of_zero_item_plus_one = part_of_zero_item[:index] + str(right)
+            first_item = part_of_first_item + part_of_zero_item_plus_one[-(step - len(part_of_first_item)):]
+            zero_item_num = int(first_item) - 1
+            if part_of_zero_item == str(zero_item_num)[-shift:]:
+                return (True, zero_item_num)
+            else:
+                return (False,)
     else:
         return (False,)
 
 def check_subsequence(sub, step):
-    """Функция возвращает номер позиции первого числа, если удалось обнаружить числовую последовательность"""
+    """Функция возвращает числа и сдвиги, если удалось обнаружить числовую последовательность"""
+    numbers = []
     for left_shift in range(1, step):
         data = check_leftshift(sub, step, left_shift)
         if data[0]:
             if check_main(sub, step, left_shift):
-                number = data[1]
-                position = get_position(number) + (len(str(number)) - left_shift) + 1
-                break
-    else:
+                numbers.append((data[1], left_shift))
+    if sub[0] != '0':
         if check_main(sub, step, 0):
-            number = int(sub[:step])
-            position = get_position(number) + 1
-            return position
-        else:
-            return 0
-    return position
+            numbers.append((int(sub[:step]), step))
+    else:
+        if step == len(sub):
+            numbers.append((int('1' + sub), 1))
+    return numbers
 
 def main():
     while True:
         A = input('Введите числовую последовательность: ')
         if A.isdigit():
-            A_leght = len(A)
-            for step in range (1, A_leght): #пробегаемся по последовательности увеличивая шаг
-                num = check_subsequence(A, step) # проверяем, можно ли уследить последовательность чисел
-                if num:
-                    break
-            else: # если ни для какого шага не обнаружили последовательность
-                if A[0] != '0': # если не начинается с нуля, то вычисляем позицию числа
-                    num = get_position(int(A)) + 1
-                else: # добавляем 1 и вычисляем позицию нового числа
-                    num = get_position(int('1' + A)) + 1
-            print(num)
+            A_lenght = len(A)
+            for step in range (1, A_lenght + 1): #пробегаемся по последовательности увеличивая шаг
+                check = check_subsequence(A, step) # получаем возможные числа
+                if check:
+                     num  = min(check)
+                     break
+            position = get_position(num[0]) + (len(str(num[0])) - num[1]) + 1
+            print(position)
         else:
             break
 
